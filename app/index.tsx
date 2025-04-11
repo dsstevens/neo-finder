a1ca0753014cedc4a51d3c6a3168645188f51b19
Deanna Stevens <deanna.stevens@atlantafalcons.nfl.com>
2025-04-11 15:51:58 -0500

    Adjust date time picker to be centered

--- app/index.tsx
+++ app/index.tsx
@@ -20,7 +20,6 @@ export default function Index() {
     try {
       setLoading(true);
       const USE_MOCK_DATA = true;
-      // const formattedDate = date.toISOString().split('T')[0];
       const data = USE_MOCK_DATA ? mockNeos : await fetchNeos(date);
       setNeos(data);
     } catch (err) {
@@ -151,8 +150,9 @@ const styles = StyleSheet.create({
     borderTopColor: '#e0e0e0',
     width: '100%',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
+    gap: 16
   },
   pickerRow: {
     flexDirection: 'row',
@@ -160,8 +160,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   datePicker: {
-    flex: 1,
-    height: 120,
+    // flex: 1,
+    // height: 120,
+    width: 120
   },
   timePicker: {
     height: 100,
